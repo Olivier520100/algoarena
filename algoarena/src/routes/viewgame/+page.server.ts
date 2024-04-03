07cd7258f4ad99c254b950afaeeee05ea1c11656
Olivier Saint-Vincent <olivier520100@gmail.com>
2024-04-03 13:32:14 -0400

    View game page created

---
+++ algoarena/src/routes/viewgame/+page.server.ts
@@ -0,0 +1,22 @@
+import type { PageServerLoad } from './$types';
+import { getXataClient } from '../../xata';
+import { redirect } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async ({ locals,depends }) => {
+    depends('data:users')
+	let session = await locals.auth();
+	if (!session?.user?.email) {
+		redirect(302, '/');
+	}
+
+   
+
+	// Function to fetch users at intervals
+	const users=await getXataClient().db.Users.sort('elo').getAll();
+	users.reverse()
+
+	
+
+	// Return the initial list of users
+	return { users:users };
+};
