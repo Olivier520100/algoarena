37d25728d69445e4e661423ffcc09adf62a18fe6
Gabriel Mikus <gabriel.mikus11@gmail.com>
2024-05-13 16:07:41 -0400

    file upload fix

--- algoarena/src/routes/dashboard/schema.ts
+++ algoarena/src/routes/dashboard/schema.ts
@@ -4,6 +4,6 @@ export const pyUploadSchema = z.object({
 	pyFile: z
 		.custom<File>()
 		.refine((f) => f && f instanceof File, 'Start by uploading a Python file.')
-		.refine((f) => f instanceof File && f.type === 'text/x-python', 'Must be a Python file.')
-		.refine((f) => f instanceof File && f.size < 10_000, 'Max 10Kb upload size.')
+		.refine((f) => f instanceof File && f.name.endsWith(".py"), 'Must be a Python file.')
+		.refine((f) => f instanceof File && f.size < 100_000, 'Max 10Kb upload size.')
 });
